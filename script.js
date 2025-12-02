7ce80cfe143660dc63c5420095695bba887c6fb7
hrsh24831-hash <hrsh24831@gmail.com>
2025-12-02 22:00:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ===========================
 // ⚠️ IMPORTANT: Replace 'something' with your actual Ngrok ID
 // Note: No space at the start of the string!
-const API_BASE = "https://fitted-events-definition-belle.trycloudflare.com";
+const API_BASE = "https://involved-cited-plant-impact.trycloudflare.com";
 
 // SHARED HEADERS (The "VIP Pass" for Ngrok)
 // We use this in every request so Ngrok doesn't show the warning page.
@@ -245,6 +245,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+
 
 
 
