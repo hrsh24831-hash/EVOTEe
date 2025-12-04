45f991c89075a194cf124279c9ab07c484e7c9e5
hrsh24831-hash <hrsh24831@gmail.com>
2025-12-04 23:32:23 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://sic-yearly-funky-prescribed.trycloudflare.com";
+const API_BASE = "https://olympic-lucy-simple-lighting.trycloudflare.com";
 
 // -----------------
 // LOGIN
@@ -191,6 +191,7 @@ function addVoter() {
             document.getElementById("v_const").value = "";
         });
 }
+
 
 
 
