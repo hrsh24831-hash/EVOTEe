b0f55d9d36526b753abc19d974be3c24c4f34cdc
hrsh24831-hash <hrsh24831@gmail.com>
2025-12-04 23:26:27 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://calm-appreciate-bestsellers-databases.trycloudflare.com";
+const API_BASE = "https://sic-yearly-funky-prescribed.trycloudflare.com";
 
 // -----------------
 // LOGIN
@@ -191,6 +191,7 @@ function addVoter() {
             document.getElementById("v_const").value = "";
         });
 }
+
 
 
 
